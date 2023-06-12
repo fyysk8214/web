e9d6693feccfc9d4d713d6f70879b5c09bd3cfdf
fyysk8214 <129704462+fyysk8214@users.noreply.github.com>
2023-06-12 10:29:14 +0900

    transition
    전환 대상, 전환 속도(초), 크기 지정
    변환 크기,회전,위치,비틀기 /translate 이동/rotate 회전/scale 크기/skew비틀기/matrix변환의 집합

---
+++ transition.js
@@ -0,0 +1,7 @@
+let di=document.getElementById("di");
+di.addEventListener("mouseover", () => {
+    di.innerText="마우스를 내려보세요"
+});
+di.addEventListener("mouseout", () => {
+    di.innerText="마우스를 올려보세요"
+});
